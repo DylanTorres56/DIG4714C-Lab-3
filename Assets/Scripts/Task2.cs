71298d78b6c013c3a537dba064af89cb7befc2bf
Dylan Torres <glaciershield@gmail.com>
2024-01-25 15:00:31 -0500

    Task 2 and GameObject Updates
    Added empty objects for tasks 2 and 3, and added a temporary TO DO section to task2 script.

--- Assets/Scripts/Task2.cs
+++ Assets/Scripts/Task2.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Task2 : MonoBehaviour
 {
+    //TO DO:
+    //Suppose that the cover price of a book is $X, but the bookstores
+    //get a 40% discount.Shipping costs $3 for the first copy and ¢75
+    //for each additional copy.Calculate the total wholescale cost and
+    //profit(before operational costs) when the bookstore sells Y copies.
+    //I want to enterthe X and Y values and get the cost and the profit
+    //as a meaningful output.
+
     // Start is called before the first frame update
     void Start()
     {
